a87a23f40d1a84a733a663cfe52b604d561b319e
Maksim Kiselev <kiselyovm@bk.ru>
2025-01-04 15:39:19 +0600

    Input System Package, Singleton

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,33 +6,15 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
 
-    [SerializeField] private float movingSpeed = 5f;
+    [SerializeField] private float movingSpeed = 10f;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate() {
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W)) {
-            inputVector.y = 1f;
-        }
-
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector.y = -1f;
-        }
-
-        if (Input.GetKey(KeyCode.A)) {
-            inputVector.x = -1f;
-        }
-
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector.x = 1f;
-        }
-
+        Vector2 inputVector = GameInput.Instance.GetMovementVector();
         inputVector = inputVector.normalized;
-
         rb.MovePosition(rb.position + inputVector * (movingSpeed * Time.fixedDeltaTime));
     }
 }
