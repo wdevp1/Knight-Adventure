07f00748f79d2c06a3057aaa9067ad27cfc3a86b
Maksim Kiselev <kiselyovm@bk.ru>
2025-01-06 15:00:14 +0600

    Animation Hero

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour
 {
@@ -19,5 +20,10 @@ public class GameInput : MonoBehaviour
         Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
 
         return inputVector;
+    }
+
+    public Vector3 GetMousePosition() {
+        Vector3 mousePos = Mouse.current.position.ReadValue();
+        return mousePos;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public static Player Instance { get; private set; }
+
     private Rigidbody2D rb;
 
     [SerializeField] private float movingSpeed = 10f;
 
+    private float minMovingSpeed = 0.1f;
+    private bool isRunning = false;
+
     private void Awake() {
+        Instance = this;
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate() {
+        HandleMovement();
+    }
+
+    private void HandleMovement() {
         Vector2 inputVector = GameInput.Instance.GetMovementVector();
         inputVector = inputVector.normalized;
         rb.MovePosition(rb.position + inputVector * (movingSpeed * Time.fixedDeltaTime));
+
+        if(Mathf.Abs(inputVector.x) > minMovingSpeed || Mathf.Abs(inputVector.y) > minMovingSpeed) {
+            isRunning = true;
+        } else {
+            isRunning = false;
+        }
+    }
+
+    public bool IsRunning() {
+        return isRunning;
+    }
+
+    public Vector3 GetPlayerScreenPosition() {
+        Vector3 playerScreenPosition = Camera.main.WorldToScreenPoint(transform.position);
+        return playerScreenPosition;
     }
 }
