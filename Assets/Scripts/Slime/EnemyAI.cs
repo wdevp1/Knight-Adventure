dfcf2dde47f4a14b879fe4ef47775f3a041b6f3f
Maksim Kiselev <kiselyovm@bk.ru>
2025-01-08 14:10:10 +0600

    Slime Animation

--- Assets/Scripts/Slime/EnemyAI.cs
+++ Assets/Scripts/Slime/EnemyAI.cs
@@ -7,6 +7,7 @@ using KnightAdventure.Utils;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] private State startingState;
+    [SerializeField] private bool guard = false;
     [SerializeField] private float roamingDistanceMax = 7f;
     [SerializeField] private float roamingDistanceMin = 3f;
     [SerializeField] private float roamingTimerMax = 2f;
@@ -18,7 +19,6 @@ public class EnemyAI : MonoBehaviour
     private Vector3 startingPosition;
 
     private enum State {
-        Idle,
         Roaming
     }
 
@@ -36,8 +36,6 @@ public class EnemyAI : MonoBehaviour
     private void Update() {
         switch(state) {
             default:
-            case State.Idle:
-                break;
             case State.Roaming:
                 roamingTime -= Time.deltaTime;
                 if(roamingTime < 0) {
@@ -49,11 +47,25 @@ public class EnemyAI : MonoBehaviour
     }
 
     private void Roaming() {
+        if(!guard) {
+            startingPosition = transform.position;
+        }
+        
         roamPosition = GetRoamingPosition();
+        ChangeFacingDirection(startingPosition, roamPosition);
         navMeshAgent.SetDestination(roamPosition);
     }
 
     private Vector3 GetRoamingPosition() {
         return startingPosition + Utils.GetRandomDir() * UnityEngine.Random.Range(roamingDistanceMin, roamingDistanceMax);
+    }
+
+    private void ChangeFacingDirection(Vector3 sourcePosition, Vector3 targetPosition) {
+        if(sourcePosition.x > targetPosition.x) {
+            transform.rotation = Quaternion.Euler(0, -180, 0);
+        }
+        else {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 }
