8057227f108efe8211d1d86ad79d1d46810bb8e9
Maksim Kiselev <kiselyovm@bk.ru>
2025-01-08 12:05:31 +0600

    Nav Mesh Components

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KnightAdventure.Utils {
+
+    public static class Utils {
+
+        public static Vector3 GetRandomDir() {
+            return new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+        }
+    }
+
+}
