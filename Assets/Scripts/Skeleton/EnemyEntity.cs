9c9b7f78bc39c9020e6a2ff92ed2413664b08c98
Maksim Kiselev <kiselyovm@bk.ru>
2025-10-03 11:30:46 +0600

    Added Skeleton dead and ScriptableObject

--- Assets/Scripts/Skeleton/EnemyEntity.cs
+++ Assets/Scripts/Skeleton/EnemyEntity.cs
@@ -1,23 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(PolygonCollider2D))]
-
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent (typeof(EnemyAI))]
 public class EnemyEntity : MonoBehaviour
 {
-    [SerializeField] private int _maxHelth = 20;
+    [SerializeField] private EnemySO _enemySO;
+
+    public event EventHandler OnTakeHit;
+    public event EventHandler OnDeath;
 
     private int _currentHelth;
 
     private PolygonCollider2D _polygonCollider2D;
+    private BoxCollider2D _boxCollider2D;
+    private EnemyAI _enemyAI;
 
     private void Awake()
     {
         _polygonCollider2D = GetComponent<PolygonCollider2D>();
+        _boxCollider2D = GetComponent<BoxCollider2D>();
+        _enemyAI = GetComponent<EnemyAI>();
     }
     private void Start() {
-        _currentHelth = _maxHelth;
+        _currentHelth = _enemySO.enemyHealth;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -27,7 +34,7 @@ public class EnemyEntity : MonoBehaviour
 
     public void TakeDamage(int damage) {
         _currentHelth -= damage;
-
+        OnTakeHit?.Invoke(this, EventArgs.Empty);
         DetectDeath();
     }
 
@@ -43,7 +50,13 @@ public class EnemyEntity : MonoBehaviour
 
     private void DetectDeath() {
         if (_currentHelth <= 0) {
-            Destroy(gameObject);
+            _polygonCollider2D.enabled = false;
+            _boxCollider2D.enabled = false;
+
+            _enemyAI.SetDeathState();
+
+            OnDeath?.Invoke(this, EventArgs.Empty);
+
         }
     }
 }
--- Assets/Scripts/Skeleton/SkeletonVisual.cs
+++ Assets/Scripts/Skeleton/SkeletonVisual.cs
@@ -1,24 +1,33 @@
+using System;
 using UnityEngine;
 
 [RequireComponent (typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class SkeletonVisual : MonoBehaviour {
 
     [SerializeField] private EnemyAI _enemyAI;
     [SerializeField] private EnemyEntity _enemyEntity;
+    [SerializeField] private GameObject _enemyShadow;
 
     private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
 
     private const string IS_RUNNING = "IsRunning";
     private const string CHASING_SPEED_MULTIPLIER = "ChasingSpeedMultiplier";
+    private const string TAKEHIT = "TakeHit";
+    private const string IS_DIE = "IsDie";
     private const string ATTACK = "Attack";
 
     private void Awake() {
         _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Start()
     {
         _enemyAI.OnEnemyAttack += _enemyAI_OnEnemyAttack;
+        _enemyEntity.OnTakeHit += _enemyEntity_OnTakeHit;
+        _enemyEntity.OnDeath += _enemyEntity_OnDeath;
     }
 
     private void OnDestroy()
@@ -44,5 +53,17 @@ public class SkeletonVisual : MonoBehaviour {
     private void _enemyAI_OnEnemyAttack(object sender, System.EventArgs e)
     {
         _animator.SetTrigger(ATTACK);
+    }
+
+    private void _enemyEntity_OnTakeHit(object sender, EventArgs e)
+    {
+        _animator.SetTrigger(TAKEHIT);
+    }
+
+    private void _enemyEntity_OnDeath(object sender, EventArgs e)
+    {
+        _animator.SetBool(IS_DIE, true);
+        _spriteRenderer.sortingOrder = -1;
+        _enemyShadow.SetActive(false);
     }
 }
